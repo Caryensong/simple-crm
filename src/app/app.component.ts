2819f008d466dba4d5599dc42b299b601fdda814
Caryensong <caryensong@googlemail.com>
2025-04-15 12:00:45 +0200

    add user dialog input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,14 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatDialogModule, MatTooltipModule, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, RouterLink],
+  imports: [CommonModule, RouterOutlet, MatDialogModule, MatInputModule,MatFormFieldModule, MatTooltipModule, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -2,16 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [CommonModule, MatDialogModule, MatFormFieldModule, MatButtonModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [CommonModule, MatDialogModule, MatFormFieldModule, MatDatepickerModule, MatButtonModule, MatInputModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
   onNoClick(){}
   animal(){}
+  onInput(){}
 }
