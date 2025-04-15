2a8e6551001cdb4053dd98bca2ec9da766a9f678
Caryensong <caryensong@googlemail.com>
2025-04-15 16:09:18 +0200

    add firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatDialogModule, MatInputModule,MatFormFieldModule, MatTooltipModule, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, RouterLink],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTooltipModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    RouterLink,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'simple-crm';
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,30 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'simple-crm-1a259',
+        appId: '1:916033567579:web:5d7bb08e3e5366dce8387e',
+        storageBucket: 'simple-crm-1a259.firebasestorage.app',
+        apiKey: 'AIzaSyBuIMs9_1SmyFjrA5NKCEZlA3EmKN9e43A',
+        authDomain: 'simple-crm-1a259.firebaseapp.com',
+        messagingSenderId: '916033567579',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
